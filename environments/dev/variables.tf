c0f775ec8c9f71bdcace04a5a98614cf671415a8
IMTO <IMTO@novonordisk.com>
2025-01-30 22:19:57 +0100

    Add section for the dev environment

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,19 @@
+# General
+variable "aws_profile" {
+  description = "The AWS profile to use"
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
+
+variable "resource_tag" {
+  description = "Resource tag for identifying resources"
+  type        = string
+}
+
 # S3
 variable "bucket_name" {
   description = "The S3 bucket name"
